681d2509822409228e25e021f1c09eb9928ab23d
TuDeSastra <widiasastra2002@gmail.com>
2022-11-04 01:33:44 +0800

    Update UTS Web

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-const searchButton = document.querySelector('#button-addon2');
-const inputKeyword = document.querySelector('.input-keyword');
-document.body.style.backgroundImage = 'url(23674-news-kasus-covid-19-menin.jpeg)'
+// CARA PENGAMBILAN 1
 
-searchButton.addEventListener('click', function() {
+const konten1 = document.getElementById("aaa");
+const konten2 = document.getElementById("bbb");
+const konten3 = document.getElementById("ccc");
+const konten4 = document.getElementById("ddd");
+document.body.style.backgroundImage = 'url(gambar/sky.jpg)'
 
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputKeyword.value+"&appid=1fe5f03e8b679377cbc41601289edfdd&units=metric")
-        .then(response => response.json())
-        .then(response => {
-            let result = document.querySelector('.result')
 
-            result.innerHTML = `<h2 style="margin-bottom: 15px;">${response.name}, ${response.sys.country}</h2>
-                                <h5><span class="temp">${response.main.temp}°С</span> <span class="temp">${response.weather[0].description}</span></h5>
-                                <p style="margin-bottom: 17px;">Temperature from ${response.main.temp_min}°С to ${response.main.temp_max}°С</p>
-                                <h5>Wind Speed : ${response.wind.speed} m/s</h5>
-                                <h5 style="margin-bottom: 17px;">Clouds : ${response.clouds.all}%</h5>
-                                <h4 style="color: #012443;">Geo Coordinates : [${response.coord.lat}, ${response.coord.lon}]</h4>` 
-        })
-    inputKeyword.value = null;
-
+$(document).ready(function(){
+    // e.preventDefault(); 
+    fetch('./stats.json')
+    .then(res => res.json())
+    .then (res =>{
+        konten1.innerHTML = `${res.name}`;
+        konten2.innerHTML = `${res.numbers.recovered}`;
+        konten3.innerHTML = `${res.numbers.infected}`;
+        konten4.innerHTML = `${res.numbers.fatal}`;
+        // console.log(res)
+    })
 })
+
